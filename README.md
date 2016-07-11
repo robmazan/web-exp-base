19d2eea5f04265eb2d8b18903818dcf4cb4bfbed
Robert Mazan <Robert_Mazan@epam.com>
2016-07-11 13:01:50 +0200

    Configured WebPack

---
+++ README.md
@@ -0,0 +1,14 @@
+# Web Experiments Base Project
+
+A `Babel` + `Sass` + `WebPack Dev Server` boilerplate.
+
+I've created this project to have something I can use when the online playgrounds are down due to service issues, or just simply suffering from high load.
+
+## Usage
+
+1. Before first run execute `npm install` to install the dependencies
+2. Execute `npm start` to start the WebPack Dev Server
+3. Open http://localhost:3000 in your browser
+4. Start playing with the `index.html`, `index.js` and `style.scss`
+
+Or you can run `npm build` to have your `.js` and `.css` files generated to the `./dist` folder.
---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+'use strict';
+const path = require('path'),
+      CleanPlugin = require('clean-webpack-plugin'),
+      autoprefixer = require('autoprefixer');
+
+const config = {
+    entry: './index.js',
+    resolve: {
+        root: __dirname,
+        modulesDirectories: [
+            'node_modules'
+        ]
+    },
+    module: {
+        loaders: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            loader: 'babel',
+            query: {
+                presets: ['es2015'],
+                plugins: ['transform-object-rest-spread']
+            }
+        }, {
+            test: /\.html$/,
+            loader: 'file',
+            query: {
+                name: '[name].[ext]'
+            }
+        }, {
+            test: /\.scss$/,
+            loader: `style-loader!css-loader!postcss-loader!sass-loader?includePaths[]=${encodeURIComponent(path.resolve(__dirname, './node_modules'))}`
+        }]
+    },
+    postcss: () => [
+        autoprefixer({ browsers: ['last 2 versions'] })
+    ],
+    output: {
+        path: 'dist',
+        publicPath: '/',
+        filename: 'script.js'
+    },
+    devServer: {
+        contentBase: 'dist'
+    },
+    plugins: [
+        new CleanPlugin(['dist'])
+    ]
+};
+
+module.exports = config;
